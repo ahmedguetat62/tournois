dd7fddc9b573c239aa0bc473a80a6ce91017bfb0
Ahmed Guetat <ahmedguetat62@gmail.com>
2024-01-08 16:47:48 +0100

    Preparing arch

---
+++ tournois_backend/src/main/java/com/example/tournois_backend/TournoisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tournois_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournoisBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TournoisBackendApplication.class, args);
+    }
+
+}
